5b2d28cd0ac5256e114cea7776c3af6419a14d9a
Ayomide Shittu <coder6lvck@gmail.com>
2022-08-15 15:49:06 +0100

    i updated the todo-app to be more fluid and to be responsive accross all platforms

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
-var entry = document.querySelector('#entry');
-var addButton = document.querySelector('#add_button');
-var output = document.querySelector(".container2");
+let entry = document.querySelector('#entry');
+let addButton = document.querySelector('#add_button');
+let output = document.querySelector(".container2");
 
 class item {
 
@@ -10,28 +9,41 @@ class item {
   }
 
 createDiv(itemName) {
-  let input= document.createElement("input");
+  let input = document.createElement("input");
   input.disabled= true;
   input.type= 'text';
-  input.value= itemName;
-  input.classList.add("ouput_value");
+  input.value = itemName;
+  input.classList.add("output_value");
+  console.log(input)
 
- /** let itemBox = document.createElement("div");
+   let itemBox = document.createElement("div");
   itemBox.classList.add("output");
 
   let editButton = document.createElement("button");
   editButton.classList.add("edit_button");
-  editButton.innerHTML= EDIT;
+  editButton.innerHTML= "EDIT";
 
   let removeButton = document.createElement("button");
-  editButton.classList.add("trash_button");
-  removeButton.innerHTML =<i class="fas fa-plus"></i>;**/
+  removeButton.classList.add("trash_button");
+  removeButton.innerHTML = "Remove";
 
-  output.appendChild(input);
+  output.appendChild(itemBox);
+  console.log(output)
+  console.log(itemBox)
 
- /** itemBox.appendChild(input);
+  itemBox.appendChild(input);
   itemBox.appendChild(editButton);
-  itemBox.appendChild(removeButton);**/
+  itemBox.appendChild(removeButton);
+  entry.value = "";
   }
 }
-new item("ayomide");   
+ 
+function handleClick(){
+  if (entry.value == ""){
+    return ;
+  } else{
+    let newItem = new item(entry.value);
+    }
+}  
+//newItem.createDiv("ayomide");
+addButton.addEventListener("click", handleClick);
